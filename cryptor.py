6a2790b4f054c0e9d74cf7e1e765ba526305768c
X_Solis_X <45941177+xsolisortusx@users.noreply.github.com>
2021-04-30 23:11:36 +0300

    Added commentaries

--- cryptor.py
+++ cryptor.py
@@ -1,22 +1,24 @@
 from cryptography.fernet import Fernet
-print("| Simple Cryptography V1 |")
-print("| By https://github.com/xsolisortusx |")
-print("| Key |")
+  # text
+  print("| Simple Cryptography V1 |")
+  print("| By https://github.com/xsolisortusx |")
+  print("| Key |")
+  # Creating Key
 cipher_key = Fernet.generate_key()
-print(cipher_key) # APM1JDVgT8WDGOWBgQv6EIhvxl4vDYvUnVdg-Vjdt0o=
-print('|     |')
+  print(cipher_key) # Doing Key for encryption
+  print('|     |')
+  # Encrypting text
 cipher = Fernet(cipher_key)
 text = input("Input: ").encode()
 encrypted_text = cipher.encrypt(text)
-print('|     Encrypted text     |')
-print(encrypted_text)
-print("|                        |")
-# (b'gAAAAABXOnV86aeUGADA6mTe9xEL92y_m0_TlC9vcqaF6NzHqRKkjEqh4d21PInEP3C9HuiUkS9f'
-# b'6bdHsSlRiCNWbSkPuRd_62zfEv3eaZjJvLAm3omnya8=')
-
+  print('|     Encrypted text     |')
+print(encrypted_text) # Printing encrypted variant of text. 
+  print("|                        |")
+  #
+  # Decrypting text [EXAMPLE OF DECRYPTION CODE]
 print("|      Decryption...     |")
 decrypted_text = cipher.decrypt(encrypted_text)
-print(decrypted_text) # 'My super secret message'
+print(decrypted_text) # Printing decrypted variant of text that you put in.
 print("|       Decrypted        |")
 # TODO:  Adding .EXE interface for using like SoftWare On Windows 10
 # If you want to send me updated version(By you) of my code you can easily do it 
