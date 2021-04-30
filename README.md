9594ace88375732bd997f0fc1ba330df89f1fcdf
decent <78281477+decent-linuxxx@users.noreply.github.com>
2021-04-30 23:33:41 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center">Welcome to SCV1 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.2-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> Very simple cryptography system. 
+
+### 🏠 [Homepage](https://scv1.zoriadev.me)
+
+## Install
+
+```sh
+pip3 install requirements.txt
+```
+
+## Usage
+
+```sh
+python3 crypto.py
+```
+
+## Author
+
+👤 **Solis [Ilia]**
+
+* Website: https://solis.zoriadev.me
+* Github: [@xsolisortusx](https://github.com/xsolisortusx)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+``__````__``
---
+++ crypto.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+from tabulate import tabulate
+from colorama import Fore, Back, Style, init
+init()
+
+method = "SCV1"
+copyrights = Style.BRIGHT + "Made by https://github.com/xsolisortusx"
+cipher_key = Fernet.generate_key()
+cipher = Fernet(cipher_key)
+text = input("Input: ").encode() or b"Nothing"
+encrypted_text = cipher.encrypt(text)
+decrypted_text = cipher.decrypt(encrypted_text)
+data = [[f"{Back.LIGHTWHITE_EX}{Fore.BLACK}{method}{Style.RESET_ALL}",
+         f"{Fore.GREEN}{cipher_key.decode()}{Style.RESET_ALL}",
+         f"{Fore.GREEN}{encrypted_text.decode()}{Style.RESET_ALL}",
+         f"{Fore.GREEN}{decrypted_text.decode()}{Style.RESET_ALL}"]]  # Data for table in line 17
+print(tabulate(data, headers=["METHOD", "KEY", "ENCRYPTED", "DECRYPTED"], tablefmt="grid"))
+print(f"\n\n{copyrights}")
+# TODO:  Adding .EXE extension support [IN PROCESS]
+# Any updates for this application are acceptable in pull requests
+# DISCORD: 𝙋𝘼𝙄𝙉𝙇𝙀𝙎𝙎#0090.
